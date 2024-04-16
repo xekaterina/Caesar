56bbdeb3473a980f4ebf33104a91bf5a07a38aab
xekaterina <145139183+xekaterina@users.noreply.github.com>
2024-04-16 13:21:21 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"strings"
+)
+
+func caesarCipher(text string, number int) string {
+	var result strings.Builder
+	number %= 26
+	for _, char := range text {
+		switch {
+		case 'a' <= char && char <= 'z':
+			result.WriteByte(byte('a' + (char-'a'+rune(number))%26))
+		case 'A' <= char && char <= 'Z':
+			result.WriteByte(byte('A' + (char-'A'+rune(number))%26))
+		default:
+			result.WriteByte(byte(char))
+		}
+	}
+	return result.String()
+}
+
+func main() {
+
+}
